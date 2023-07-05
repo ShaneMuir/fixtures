f1dcf85202984683a35f0a06e39989047f359c30
Shane Muirhead <shane.muirhead@tribusdigital.com>
2023-07-05 10:30:17 +0100

    updated so teams can't play home and away on the same day and updated format

--- fixtures.py
+++ fixtures.py
@@ -63,18 +63,23 @@ def generate_fixture_list(weeks_available, match_day, match_time, divisions):
         club_combinations = list(itertools.combinations(clubs, 2))
         for week in range(weeks_available):
             week_fixtures = []
+            scheduled_clubs = set()
             for home_team, away_team in club_combinations:
-                home_tables = division['clubs'][home_team]
-                away_tables = division['clubs'][away_team]
-                if home_tables > 0 and away_tables > 0:
-                    week_fixtures.append((home_team, away_team))
-                    division['clubs'][home_team] -= 1
-                    division['clubs'][away_team] -= 1
+                if home_team not in scheduled_clubs and away_team not in scheduled_clubs:
+                    home_tables = division['clubs'][home_team]
+                    away_tables = division['clubs'][away_team]
+                    if home_tables > 0 and away_tables > 0:
+                        week_fixtures.append((home_team, away_team))
+                        division['clubs'][home_team] -= 1
+                        division['clubs'][away_team] -= 1
+                        scheduled_clubs.add(home_team)
+                        scheduled_clubs.add(away_team)
             fixtures.append((division_name, week + 1, week_fixtures))
             # reset table availability for next week
             for club in clubs:
                 division['clubs'][club] = division['original_clubs'][club]
     return fixtures
+
 
 
 def get_match_dates(start_date, weeks_available, match_day, match_time):
@@ -109,32 +114,30 @@ def get_match_dates(start_date, weeks_available, match_day, match_time):
 
 
 def write_to_csv(fixtures, match_dates, filename):
+    division_names = list(divisions.keys())
     max_week_fixtures = max([len(fixture[2]) for fixture in fixtures])
-    weeks_per_row = 3
     with open(filename, 'w') as f:
         # Write header row
-        for i in range(weeks_per_row):
-            f.write('Division,Week,Date,Home Team,Away Team')
-            if i < weeks_per_row - 1:
-                f.write(',')
+        for division_name in division_names:
+            f.write(f'Division,Week,Date,Home Team,Away Team')
+            if division_name != division_names[-1]:
+                f.write(',,,,')
         f.write('\n')
 
         # Write fixtures
-        for row in range(0, weeks_available, weeks_per_row):
+        for week in range(weeks_available):
             for i in range(max_week_fixtures):
-                for week_offset in range(weeks_per_row):
-                    week = row + week_offset
-                    if week < weeks_available:
-                        fixture = fixtures[week]
-                        division_name, _, week_fixtures = fixture
-                        if i < len(week_fixtures):
-                            home_team, away_team = week_fixtures[i]
-                            match_date = match_dates[week].strftime('%d-%m-%Y %I:%M %p')
-                            f.write(f'{division_name},{week + 1},{match_date},{home_team},{away_team}')
-                        else:
-                            f.write(',,,,')
-                    if week_offset < weeks_per_row - 1:
-                        f.write(',')
+                for division_name in division_names:
+                    fixture = [f for f in fixtures if f[0] == division_name and f[1] == week + 1][0]
+                    _, _, week_fixtures = fixture
+                    if i < len(week_fixtures):
+                        home_team, away_team = week_fixtures[i]
+                        match_date = match_dates[week].strftime('%d-%m-%Y %I:%M %p')
+                        f.write(f'{division_name},{week + 1},{match_date},{home_team},{away_team}')
+                    else:
+                        f.write(',,,,')
+                    if division_name != division_names[-1]:
+                        f.write(',,,,')
                 f.write('\n')
 
 
