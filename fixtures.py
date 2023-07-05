12fa40c4c74097cf3bf43df3e44c367c83629d59
Shane Muirhead <shane.muirhead@tribusdigital.com>
2023-07-05 16:57:48 +0100

    improved the dict and fixutres algorithm again

--- fixtures.py
+++ fixtures.py
@@ -6,60 +6,67 @@ divisions = {
         "clubs": {
             "Club 1": {
                 "teams": [
-                    "team_one"
+                    "team_1A",
                 ],
                 "tables": 1
             },
             "Club 2": {
                 "teams": [
-                    "team_one"
+                    "team_2A"
                 ],
                 "tables": 1
             },
             "Club 3": {
                 "teams": [
-                    "team_one"
+                    "team_3A"
                 ],
                 "tables": 1
             },
             "Club 4": {
                 "teams": [
-                    "team_one",
-                    "team_two"
+                    "team_4A",
+                    "team_4B"
                 ],
                 "tables": 2
             },
             "Club 5": {
                 "teams": [
-                    "team_one"
+                    "team_5A"
                 ],
                 "tables": 1
             },
             "Club 6": {
                 "teams": [
-                    "team_one",
-                    "team_two",
-                    "team_three"
+                    "team_6A",
+                    "team_6B",
+                    "team_6C"
                 ],
                 "tables": 3
             },
             "Club 7": {
                 "teams": [
-                    "team_one"
+                    "team_7A"
                 ],
                 "tables": 1
             },
             "Club 8": {
                 "teams": [
-                    "team_two"
+                    "team_8A"
                 ],
                 "tables": 1
             },
             "Club 9": {
                 "teams": [
-                    "team_one"
+                    "team_9A",
+                    "team_9B"
                 ],
-                "tables": 1
+                "tables": 2
+            },
+            "Club 10": {
+                "teams": [
+                    "DUMMY",
+                ],
+                "tables": 0
             }
         },
         "original_clubs": {
@@ -71,62 +78,70 @@ divisions = {
             "Club 6": 3,
             "Club 7": 1,
             "Club 8": 1,
-            "Club 9": 1
+            "Club 9": 1,
+            "Club 10": 0
         }
     },
     "Division 2": {
         "clubs": {
             "Club 10": {
                 "teams": [
-                    "team_one"
+                    "team_10A",
+                    "team_10B"
                 ],
                 "tables": 1
             },
             "Club 11": {
                 "teams": [
-                    "team_one"
+                    "team_11A"
                 ],
                 "tables": 1
             },
             "Club 12": {
                 "teams": [
-                    "team_one",
-                    "team_two"
+                    "team_12A",
+                    "team_12B"
                 ],
                 "tables": 2
             },
             "Club 13": {
                 "teams": [
-                    "team_one"
+                    "team_13A"
                 ],
                 "tables": 1
             },
             "Club 4": {
                 "teams": [
-                    "team_one",
-                    "team_two"
+                    "team_4D",
                 ],
                 "tables": 2
             },
             "Club 6": {
                 "teams": [
-                    "team_one",
-                    "team_two",
-                    "team_three"
+                    "team_6C",
+                    "team_6D",
+                    "team_6E"
                 ],
                 "tables": 3
             },
             "Club 14": {
                 "teams": [
-                    "team_one"
+                    "team_14A",
+                    "team_14B"
                 ],
                 "tables": 1
             },
             "Club 15": {
                 "teams": [
-                    "team_one"
+                    "team_15A"
                 ],
                 "tables": 1
+            },
+            "Club 16": {
+                "teams": [
+                    "DUMMY"
+                ],
+                "tables": 0
             }
         },
         "original_clubs": {
@@ -137,7 +152,8 @@ divisions = {
             "Club 4": 2,
             "Club 6": 3,
             "Club 14": 1,
-            "Club 15": 1
+            "Club 15": 1,
+            "Club 16": 0,
         }
     }
 }
@@ -148,6 +164,9 @@ match_time = "8:00 PM"
 start_date = datetime(2023, 9, 7)  # Start 7th of September
 
 
+import random
+
+
 def generate_fixture_list(weeks_available, match_day, match_time, divisions):
     fixtures = []
     for division_name, division in divisions.items():
@@ -155,34 +174,53 @@ def generate_fixture_list(weeks_available, match_day, match_time, divisions):
         club_combinations = []
         for club_name, club in division['clubs'].items():
             for team_name in club['teams']:
+                home_team = f'{club_name} {team_name}'
                 for opponent_club_name, opponent_club in division['clubs'].items():
                     if club_name != opponent_club_name:
                         for opponent_team_name in opponent_club['teams']:
-                            club_combinations.append((f'{club_name} {team_name}', f'{opponent_club_name} {opponent_team_name}'))
+                            away_team = f'{opponent_club_name} {opponent_team_name}'
+                            club_combinations.append((home_team, away_team))
         scheduled_games = {}
         for week in range(weeks_available):
             week_fixtures = []
             scheduled_teams = set()
+            # shuffle the list of games before scheduling them
+            random.shuffle(club_combinations)
             for home_team, away_team in club_combinations:
                 if home_team not in scheduled_teams and away_team not in scheduled_teams:
+                    # check if either team is already scheduled to play this week
+                    if any(home_team in game or away_team in game for game in week_fixtures):
+                        continue
+                    # check if there are enough tables available at both clubs
                     home_club_name = ' '.join(home_team.split(' ')[:-1])
                     away_club_name = ' '.join(away_team.split(' ')[:-1])
                     home_tables = division['clubs'][home_club_name]['tables']
                     away_tables = division['clubs'][away_club_name]['tables']
-                    if home_tables > 0 and away_tables > 0:
-                        game_key_1 = f'{home_team}-{away_team}'
-                        game_key_2 = f'{away_team}-{home_team}'
-                        if game_key_1 not in scheduled_games and game_key_2 not in scheduled_games:
-                            week_fixtures.append((home_team, away_team))
-                            scheduled_teams.add(home_team)
-                            scheduled_teams.add(away_team)
-                            scheduled_games[game_key_1] = True
-                        elif game_key_2 not in scheduled_games:
-                            week_fixtures.append((away_team, home_team))
-                            scheduled_teams.add(home_team)
-                            scheduled_teams.add(away_team)
-                            scheduled_games[game_key_2] = True
+                    if home_tables < 1 or away_tables < 1:
+                        continue
+                    game_key_1 = f'{home_team}-{away_team}'
+                    game_key_2 = f'{away_team}-{home_team}'
+                    if game_key_1 not in scheduled_games and game_key_2 not in scheduled_games:
+                        week_fixtures.append((home_team, away_team))
+                        scheduled_teams.add(home_team)
+                        scheduled_teams.add(away_team)
+                        scheduled_games[game_key_1] = True
+                    elif game_key_2 not in scheduled_games:
+                        week_fixtures.append((away_team, home_team))
+                        scheduled_teams.add(home_team)
+                        scheduled_teams.add(away_team)
+                        scheduled_games[game_key_2] = True
+            # check if there are fewer than 7 games this week
+            if len(week_fixtures) < 7:
+                print(f'Week {week + 1}: only {len(week_fixtures)} games scheduled')
+                unscheduled_teams = set(clubs) - scheduled_teams
+                print(f'Unscheduled teams: {unscheduled_teams}')
+                unscheduled_games = [game for game in club_combinations if game[0] in unscheduled_teams or game[1] in unscheduled_teams]
+                print(f'Unscheduled games: {unscheduled_games}')
             fixtures.append((division_name, week + 1, week_fixtures))
+            # reset table availability for next week
+            for club in clubs:
+                division['clubs'][club]['tables'] = division['original_clubs'][club]
     return fixtures
 
 
